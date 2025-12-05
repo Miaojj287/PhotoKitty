060e5b08ad0781278926332009e865d3db70cfdf
Etytech <820932622@qq.com>
2025-12-05 03:21:37 -0800

    Happy Birthday.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  // 开发环境使用根路径，生产环境使用 /PhotoKitty/
+  base: command === 'build' ? '/PhotoKitty/' : '/',
+}))
